dbbdd6c96c67a270c5ce2b0f38a53b5cd2500ffa
DEV Ivair Quirino <dev.ivairquirino@gmail.com>
2025-05-20 20:47:55 -0300

    exemplo array de args

---
+++ exemplos-curso-java-dio/src/AboutMe.java
@@ -0,0 +1,11 @@
+public class AboutMe {
+    public static void main(String[] args){
+        String nome= args[0];
+        String sobrenome= args[1];
+        String idade= args[2];
+        String altura= args[3];
+        System.out.println("Nome: "+nome+" "+sobrenome+"\n");
+        System.out.println("Idade: "+idade+"\n");
+        System.out.println("Altura: "+altura+"\n");
+    }
+}
