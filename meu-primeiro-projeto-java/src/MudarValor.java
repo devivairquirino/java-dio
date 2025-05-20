475ce8225273177a838cb7a8a4d8069f1cb9a0ff
DEV Ivair Quirino <dev.ivairquirino@gmail.com>
2025-05-20 13:08:24 -0300

    add mudar valor de variavel

--- meu-primeiro-projeto-java/src/MudarValor.java
+++ meu-primeiro-projeto-java/src/MudarValor.java
@@ -1,3 +1,16 @@
 public class MudarValor {
-    
+    public static void main(String[] args) {
+        int n1= 10;
+        int n2= 3;
+        int auxiliar;
+        System.out.println("n1= " + n1 + " >> n2= " + n2);
+
+        // O valor n1 vai ser armazenado em n2 e vice versa
+        auxiliar= n1;
+        n1= n2;
+        n2= auxiliar;
+
+        System.out.println("n1= " + n1 + " >> n2= " + n2);
+
+    }
 }
