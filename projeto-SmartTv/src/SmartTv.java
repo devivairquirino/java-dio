99b9f4ae5debea0593b21833a01221bab20371e7
DEV Ivair Quirino <dev.ivairquirino@gmail.com>
2025-05-20 15:03:03 -0300

    add funcões na smartTv

--- projeto-SmartTv/src/SmartTv.java
+++ projeto-SmartTv/src/SmartTv.java
@@ -4,11 +4,75 @@ public class SmartTv {
     int volume= 10;
 
     public void ligarTv(){
-        ligada= true;
-        System.out.println("Ligando SmartTv...");
+        if(ligada){
+            System.out.println("A SmartTv já está ligada!");
+        }
+        else{
+            ligada= true;
+            System.out.println("Ligando SmartTv...");
+        }
     }
     public void desligarTv(){
-        ligada= false;
-        System.out.println("Desligando SmartTv...");  
+        if(!ligada){
+            System.out.println("A SmartTv já está desligada!");
+        }
+        else{
+            ligada= false;
+            System.out.println("Desligando SmartTv...");  
+        }
+    }
+    public void aumentarVolume(){
+        if(!ligada){
+            System.out.println("SmartTv Desligada, por favor ligue-a!");
+        }
+        else{
+            volume++;
+            System.out.println("Volume+ : "+volume); 
+        }
+    }
+    public void diminuirVolume(){
+        if(!ligada){
+            System.out.println("SmartTv Desligada, por favor ligue-a!");
+        }
+        else{
+            volume--;
+            System.out.println("Volume- : "+volume); 
+        }  
+    }
+    public void aumentarCanal(){
+        if(!ligada){
+            System.out.println("SmartTv Desligada, por favor ligue-a!");
+        }
+        else{
+            canal++;
+            System.out.println("Canal+ : "+canal); 
+        }   
+    }
+    public void diminuirCanal(){
+        if(!ligada){
+            System.out.println("SmartTv Desligada, por favor ligue-a!");
+        }
+        else{
+            canal--;
+            System.out.println("Canal- : "+canal); 
+        }  
+    }
+    public void mudarCanal(int novoCanal){
+        if(!ligada){
+            System.out.println("SmartTv Desligada, por favor ligue-a!");
+        }
+        else{
+            canal= novoCanal;
+            System.out.println("Canal : "+canal); 
+        }
+    }
+    // imprime status da smartTv
+    public void mostrarStatusSmartTv(){
+        System.out.println(" ");
+        System.out.println("SmartTv Status >>>>>>>>>>>>>>");
+        System.out.println("Ligada: "+ligada);
+        System.out.println("Canal: "+canal);
+        System.out.println("Volume: "+volume);
+        System.out.println(" ");
     }
 }
--- projeto-SmartTv/src/Usuario.java
+++ projeto-SmartTv/src/Usuario.java
@@ -1,11 +1,23 @@
 public class Usuario {
     public static void main(String[] args) throws Exception {
         SmartTv smartTv= new SmartTv();
+        //Comandos volume e canal so funcionam se a tv estiver ligada
+        smartTv.aumentarVolume();
+        smartTv.mudarCanal(9);
 
         smartTv.ligarTv();
-        System.out.println("SmartTv Status >>>>>>>>>>>>>>");
-        System.out.println("Ligada: "+smartTv.ligada);
-        System.out.println("Canal: "+smartTv.canal);
-        System.out.println("Volume: "+smartTv.volume);
+
+        smartTv.mostrarStatusSmartTv();
+
+        
+        smartTv.aumentarVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarCanal();
+        smartTv.aumentarCanal();
+        smartTv.diminuirCanal();
+        smartTv.mudarCanal(24);
+
+        smartTv.mostrarStatusSmartTv();
+
     }
 }
