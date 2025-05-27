2ef35bb8ece117fdfdf0862f5b8d5a15c5a45726
DEV Ivair Quirino <dev.ivairquirino@gmail.com>
2025-05-27 12:43:42 -0300

    implement basic banking functionality with deposit, withdrawal, and balance inquiry

--- meu-primeiro-projeto-java/src/App.java
+++ meu-primeiro-projeto-java/src/App.java
@@ -1,5 +1,47 @@
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello World!");
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double saldo = 0;
+        boolean continuar = true;
+
+        while (continuar) {
+
+            int opcao = scanner.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    // TODO: Ler o valor a ser depositado e atualizar/imprimir o saldo.
+                    System.out.println("Qual valor deseja depositar? ");
+                    saldo+= scanner.nextDouble();
+                    System.out.printf("Saldo atual: %.1f", saldo);
+                    break;
+                case 2:
+                    // TODO: Ler o valor a ser sacado e verificar/imprimir se há saldo suficiente.
+                    System.out.println("Qual valor deseja sacar? ");
+                    double saque= scanner.nextDouble();
+                    if(saque<=saldo){
+                      saldo-= saque;
+                      System.out.printf("Saldo atual: %.1f", saldo);
+                    }
+                    else{
+                      System.out.println("Saldo insuficiente.");
+                    }
+                    break;
+                case 3:
+                    // TODO: Exibir o saldo atual da conta.
+                    System.out.printf("Saldo atual: %.1f", saldo);
+                    break;
+                case 0:
+                    System.out.println("Programa encerrado.");
+                    continuar = false;  // Atualiza a variável de controle para encerrar o loop
+                    break;
+                default:
+                    System.out.println("Opção inválida. Tente novamente.");
+            }
+        }
+        scanner.close();
     }
 }
